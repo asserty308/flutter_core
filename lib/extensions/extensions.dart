daadad95a418c5429ad926eef075b52632e56bfd
asserty308 <yannickassert@gmail.com>
2025-01-22 15:29:44 +0100

    chore: update exports

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,5 @@
+export 'build_context.dart';
+export 'date_time.dart';
+export 'duration.dart';
+export 'time_of_day.dart';
+export 'widget.dart';
--- lib/flutter_core.dart
+++ lib/flutter_core.dart
@@ -1,7 +1,4 @@
 library;
 
-export 'extensions/build_context.dart';
-export 'extensions/date.dart';
-export 'extensions/widget.dart';
-
+export 'extensions/extensions.dart';
 export 'ui/widgets/gaps.dart';
