4b8a8fa3ecc1e2701ac0589098795d4121ab735d
asserty308 <yannickassert@gmail.com>
2025-01-22 15:38:53 +0100

    feat: add AdaptiveDialogButton widget

---
+++ lib/ui/widgets/adaptive_dialog_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_core/flutter_core.dart';
+
+class AdaptiveDialogButton extends StatelessWidget {
+  const AdaptiveDialogButton({
+    super.key,
+    required this.onPressed,
+    required this.child,
+  });
+
+  final void Function() onPressed;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => switch (context.theme.platform) {
+    TargetPlatform.iOS || TargetPlatform.macOS => CupertinoDialogAction(
+      onPressed: onPressed,
+      child: child,
+    ),
+    _ => TextButton(
+      onPressed: onPressed,
+      child: child,
+    ),
+  };
+}
