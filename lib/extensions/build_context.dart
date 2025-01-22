3a321a40153590f0b42b4da151466912450fc74f
asserty308 <yannickassert@gmail.com>
2025-01-22 15:24:45 +0100

    feat: add mediaPadding and focusScope extensions to BuildContext

--- lib/extensions/build_context.dart
+++ lib/extensions/build_context.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 extension BuildContextExtension on BuildContext {
   // Access to MediaQuery
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+  EdgeInsets get mediaPadding => mediaQuery.padding;
 
   // Access to Theme
   ThemeData get theme => Theme.of(this);
@@ -16,8 +17,17 @@ extension BuildContextExtension on BuildContext {
   // Orientation shortcut
   Orientation get orientation => mediaQuery.orientation;
 
-  // Example of screen width and height for convenience
+  // Screen size shortcuts
   Size get mediaSize => MediaQuery.sizeOf(this);
   double get screenWidth => mediaSize.width;
   double get screenHeight => mediaSize.height;
+
+  // Access to focus scpoe
+  FocusScopeNode get focusScope => FocusScope.of(this);
+
+  void dismissKeyboard() {
+    if (!focusScope.hasPrimaryFocus) {
+      focusScope.unfocus();
+    }
+  }
 }
