2289593ae5b78c9940f3eea49217f4c6b4a930a5
asserty308 <yannickassert@gmail.com>
2025-03-27 15:35:10 +0100

    feat: add app state

---
+++ lib/src/ui/widgets/app_state.dart
@@ -0,0 +1,63 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// App Lifecycle: Running -> onIncactive (App list) -> onHide -> onPause -> onRestart -> onShow -> onResume
+mixin _AppLifecycleMixin<T extends StatefulWidget> on State<T> {
+  late final AppLifecycleListener _lifecycleListener;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lifecycleListener = AppLifecycleListener(
+      onResume: onResume,
+      onInactive: onInactive,
+      onHide: onHide,
+      onShow: onShow,
+      onPause: onPause,
+      onRestart: onRestart,
+      onDetach: onDetach,
+      onStateChange: onStateChange,
+      onExitRequested: onExitRequested,
+    );
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => onUIReady());
+  }
+
+  @override
+  void dispose() {
+    _lifecycleListener.dispose();
+    super.dispose();
+  }
+
+  void onUIReady() {}
+
+  void onResume() {}
+
+  void onInactive() {}
+
+  void onHide() {}
+
+  void onShow() {}
+
+  void onPause() {}
+
+  void onRestart() {}
+
+  void onDetach() {}
+
+  void onStateChange(AppLifecycleState state) {}
+
+  Future<AppExitResponse> onExitRequested() {
+    return Future.value(AppExitResponse.exit);
+  }
+}
+
+abstract class AppState<T extends StatefulWidget> extends State<T>
+    with _AppLifecycleMixin<T> {}
+
+abstract class AppConsumerState<T extends ConsumerStatefulWidget>
+    extends ConsumerState<T>
+    with _AppLifecycleMixin<T> {}
